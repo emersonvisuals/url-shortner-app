970d34a814fe132db00997d315f862531230eb5b
Thomas Emerson <emersonvisuals@gmail.com>
2022-09-11 20:22:40 +1000

    new

--- src/index.js
+++ src/index.js
@@ -8,36 +8,36 @@ import bgShortenDesktop from './assets/bg-shorten-desktop.svg'
 import bgShortenMobile from './assets/bg-shorten-mobile.svg'
 
 import iconBrandRecognition from './assets/icon-brand-recognition.svg'
-const brandRecognitionSrc = document.querySelector('.iconOne')
-brandRecognitionSrc.src = iconBrandRecognition
+const iconOne = document.querySelector('.iconOne')
+iconOne.src = iconBrandRecognition
 
 import iconDetailedRecords from './assets/icon-detailed-records.svg'
-const detailedRecordsSrc = document.querySelector('.iconTwo')
-detailedRecordsSrc.src = iconDetailedRecords
+const iconTwo = document.querySelector('.iconTwo')
+iconTwo.src = iconDetailedRecords
 
 import iconFullyCustomizable from './assets/icon-fully-customizable.svg'
-const fullyCustomizableSrc = document.querySelector('.iconThree')
-fullyCustomizableSrc.src = iconFullyCustomizable
+const iconThree = document.querySelector('.iconThree')
+iconThree.src = iconFullyCustomizable
 
 import iconFacebook from './assets/icon-facebook.svg'
-const facebookSrc = document.querySelector('.facebook')
-facebookSrc.src = iconFacebook
+const facebook = document.querySelector('.facebook')
+facebook.src = iconFacebook
 
 import iconInstagram from './assets/icon-instagram.svg'
-const instagramSrc = document.querySelector('.instagram');
-instagramSrc.src = iconInstagram
+const instagram = document.querySelector('.instagram');
+instagram.src = iconInstagram
 
 import iconPinterest from './assets/icon-pinterest.svg'
-const pinterestSrc = document.querySelector('.pinterest');
-pinterestSrc.src = iconPinterest
+const pinterest = document.querySelector('.pinterest');
+pinterest.src = iconPinterest
 
 import iconTwitter from './assets/icon-twitter.svg'
-const twitterSrc = document.querySelector('.twitter')
-twitterSrc.src = iconTwitter
+const twitter = document.querySelector('.twitter')
+twitter.src = iconTwitter
 
-import illustration from './assets/illustration-working.svg'
-const illustrationSrc = document.querySelector('.illustration')
-illustrationSrc.src = illustration
+import illustrationImport from './assets/illustration-working.svg'
+const illustration = document.querySelector('.illustration')
+illustration.src = illustrationImport
 
 import logoImport from './assets/logo.svg'
 const logo = document.getElementById('logo');
