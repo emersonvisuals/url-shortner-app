f1894f24948bc246173e83ec1cd99a778f1cb1de
Thomas Emerson <emersonvisuals@gmail.com>
2022-09-11 23:53:52 +1000

    new test

--- src/warning.js
+++ src/warning.js
@@ -1,16 +1,16 @@
 function warning() {
     const link = document.querySelector('input.textBox');
     const errorMessage = document.querySelector('.warning');
-    const linkPlaceholder = document.querySelector('input.textBox::placeholder');
+    // const linkPlaceholder = document.querySelector('input.textBox::placeholder');
 
     if (link.value === '') {
         link.classList.add('active')
         errorMessage.classList.add('active')
-        linkPlaceholder.classList.add('active');
+        // linkPlaceholder.classList.add('active');
     } else {
         link.classList.remove('active');
         errorMessage.classList.remove('active');
-        linkPlaceholder.classList.remove('active');
+        // linkPlaceholder.classList.remove('active');
     }
 }
 
