1811a6562235974c7266ee2f3805930a5d5af754
Thomas Emerson <emersonvisuals@gmail.com>
2022-09-11 20:29:56 +1000

    new test 2

--- src/index.js
+++ src/index.js
@@ -8,41 +8,43 @@ import bgShortenDesktop from './assets/bg-shorten-desktop.svg'
 import bgShortenMobile from './assets/bg-shorten-mobile.svg'
 
 import iconBrandRecognition from './assets/icon-brand-recognition.svg'
+import iconDetailedRecords from './assets/icon-detailed-records.svg'
+import iconFullyCustomizable from './assets/icon-fully-customizable.svg'
+import iconFacebook from './assets/icon-facebook.svg'
+import iconInstagram from './assets/icon-instagram.svg'
+import iconPinterest from './assets/icon-pinterest.svg'
+import iconTwitter from './assets/icon-twitter.svg'
+import illustrationImport from './assets/illustration-working.svg'
+import logoImport from './assets/logo.svg'
+import footerLogoImport from './assets/logo copy.svg'
+
+
 const iconOne = document.querySelector('.iconOne')
 iconOne.src = iconBrandRecognition
 
-import iconDetailedRecords from './assets/icon-detailed-records.svg'
 const iconTwo = document.querySelector('.iconTwo')
 iconTwo.src = iconDetailedRecords
 
-import iconFullyCustomizable from './assets/icon-fully-customizable.svg'
 const iconThree = document.querySelector('.iconThree')
 iconThree.src = iconFullyCustomizable
 
-import iconFacebook from './assets/icon-facebook.svg'
 const facebook = document.querySelector('.facebook')
 facebook.src = iconFacebook
 
-import iconInstagram from './assets/icon-instagram.svg'
 const instagram = document.querySelector('.instagram');
 instagram.src = iconInstagram
 
-import iconPinterest from './assets/icon-pinterest.svg'
 const pinterest = document.querySelector('.pinterest');
 pinterest.src = iconPinterest
 
-import iconTwitter from './assets/icon-twitter.svg'
 const twitter = document.querySelector('.twitter')
 twitter.src = iconTwitter
 
-import illustrationImport from './assets/illustration-working.svg'
 const illustration = document.querySelector('.illustration')
 illustration.src = illustrationImport
 
-import logoImport from './assets/logo.svg'
 const logo = document.getElementById('logo');
 logo.src = logoImport
 
-import footerLogoImport from './assets/logo copy.svg'
 const footerLogo = document.querySelector('.footerLogo');
 footerLogo.src = footerLogoImport
